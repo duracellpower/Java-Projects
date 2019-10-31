a25669f412df0da08b8df9f0318ec8b65dc471b5
duracellpower <34420517+duracellpower@users.noreply.github.com>
2019-10-31 22:01:12 +0100

    wip

--- 226b/location/src/main/java/com/bharath/location/repos/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bharath.location.repos;
-
-import com.bharath.location.entities.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Integer> {
-}
